bd89edc5a5e76ebde1570f6dbb0126604cb2426c
mr.xie <xieyongjian_web@163.com>
2019-03-24 23:45:15 +0800

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       host : '47.107.150.71',
       ref  : 'origin/master',
       repo : 'git@github.com:sixgay/pm2_test.git',
-      path : '/home/pm2_demo',
+      path : '/ftp/node/production',
       'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
